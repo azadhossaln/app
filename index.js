fbcb0ab2c5b981472261b013cc6ba8691332ec91
azadhossaln <49028214+azadhossaln@users.noreply.github.com>
2024-01-28 20:33:40 +0600

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const app = express();
 const PORT = process.env.PORT || 3030;
 
 // your code
+app.get("*", (req, res, next) => {
+  res.send("I am Alive");
+})
 
 app.listen(PORT, () => {
   console.log(`server started on port ${PORT}`);
